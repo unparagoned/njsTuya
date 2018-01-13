5a84fc3caa38400819710286aad38f2523631093
unparagoned <unparagoned@msn.com>
2018-01-13 20:18:55 +0000

    now working with api, and config rule example

--- scripts/njstuya.js
+++ scripts/njstuya.js
@@ -9,6 +9,8 @@ args can be, ON, OFF, or TOGGLE. No arguement returns state
 
 const TuyaDevice = require('tuyapi');
 var args = process.argv.slice(2);
+_DEBUG = false;
+var db = _DEBUG;
 
 function getArgs(allArgs, argName) {
     var nameIndex = allArgs.indexOf(argName);
@@ -20,8 +22,8 @@ var tuyaIP = getArgs(args, "-ip");
 var tuyaID = getArgs(args, "-id");
 var tuyaKey = getArgs(args, "-key");
 var tuya = new TuyaDevice({
-    type: 'outlet',
-    ip: tuyaIP,
+    //   type: 'outlet',
+    //   ip: tuyaIP,
     id: tuyaID,
     key: tuyaKey
 });
@@ -31,32 +33,29 @@ function bmap(istate) {
 }
 
 var newState = false;
+var changeState = false;
 
-tuya.get(function(error, status) {
-    if (error) { return console.log(error); }
-    //console.log('Start status: ' + bmap(status));
+tuya.resolveIds().then(() => {
+    tuya.get().then(status => {
+        if (db) { console.log('Status: ' + status); }
+        newState = status;
+        if (args.includes("ON")) {
+            newState = true;
+            changeState = true;
+        }
+        if (args.includes("OFF")) {
+            newState = false;
+            changeState = true;
+        }
+        if (args.includes("TOGGLE")) {
+            newState = !status;
+            changeState = true;
+        }
 
-    if (args.includes("ON")) {
-        newState = true;
-    }
-    if (args.includes("OFF")) {
-        newState = false;
-    }
-    if (args.includes("TOGGLE")) {
-        newState = !status;
-    }
-    tuya.set(newState, function(error, result) {
-        if (error) { return console.log(error); }
-        //console.log('Set status to ' + bmap(newState) + ': ' + bmap(result));
-
-        tuya.get(function(error, status) {
-            if (error) { return console.log(error); }
-            // console.log('End state: ' + [status ? 'on' : 'off']);
-            console.log(bmap(status));
-            tuya.destroy();
-            // process.exitCode = [status ? 1 : 0]; Exec takes value of console output so no need for this
+        tuya.set({ set: newState }).then(result => {
+            if (db) { console.log('Result of setting status to ' + newState + ': ' + result); }
+            console.log(bmap(result));
+            return;
         });
-
     });
-
 });
